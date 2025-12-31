21b2ebcb509e6104eead8337b6b86fd6b6aab057
Arun Sabu <sabuarun00@gmail.com>
2025-12-31 16:23:22 +0530

    Add Netlify SPA redirects

--- src/pages/NewPassword.jsx
+++ src/pages/NewPassword.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import Loader from "../components/Loader";
 import axios from "../api/axios";
-import "../Authpage.css";
+import "../AuthPage.css";
 
 function SetNewPassword() {
   // only token needed
